505795275c664b225e58ed892008ad522a812f2d
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-05-09 10:03:09 -0400

    Adding code for Routes and Models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,39 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  through: {
+    model: Tag,
+    unique: false
+  },
 
+
+});
 // Categories have many Products
 
+Category.belongsToMany(Product, {
+
+  through: {
+    model: Tag,
+    unique: false
+  },
+
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+});
 module.exports = {
   Product,
   Category,
